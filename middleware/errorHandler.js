12641760a56c1d79427266c52589b4aaf8a28d38
unknown <anjarulrobin@gmail.com>
2020-02-12 11:10:55 +0600

    Showing Error message to error view

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,12 +1,6 @@
 const bookHandler = require('../models/book');
 
 module.exports = async function(err, req, res, next){
-    const books = await bookHandler.showBook();    
-    const data = {        
-        books,
-        message: err.message,
-        link_name   : ['/login', '/register'],
-        link_msg    : ['Login', 'Register']     
-    }
-    res.header(403).render('home', {data: data});
+    err.message = "Unauthorized access!";
+    res.header(403).render('error', {error: err});
 }
