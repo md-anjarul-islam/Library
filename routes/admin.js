a57a96c2284f4f550353eba88010e97c9c356829
unknown <anjarulrobin@gmail.com>
2020-02-12 11:12:22 +0600

    authentication and errorhandler middleware applied to admin router

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controller/adminController');
+const auth = require('../middleware/auth');
+const errorHandler = require('../middleware/errorHandler');
+
+router.use([auth, errorHandler]);
 
 router.get('/alluser', adminController.getAllUser);
 router.get('/allbook', adminController.getAllBook);
