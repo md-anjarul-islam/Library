2a784e72f391f480b487c071d03f3cac1655e8c1
unknown <anjarulrobin@gmail.com>
2020-02-13 18:34:07 +0600

    applied form validation middleware to index.js and user.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const indexController = require('../controller/indexController');
+const validator = require('../middleware/validator');
 
 router.get('/', indexController.getHome);
 router.get('/login', indexController.getLogin);
 router.get('/logout', indexController.getLogout);
 router.get('/register', indexController.getRegister);
 
-router.post('/login', indexController.postLogin);
-router.post('/register', indexController.postRegister);
+router.post('/login', validator.loginFormValidate, indexController.postLogin);
+router.post('/register', validator.regFormValidate, indexController.postRegister);
 router.post('/searchbook', indexController.postSearchBook);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
-const path = require('path');
 const upload = require('../util/multer-upload');
 const auth = require('../middleware/auth');
 const errorHandler = require('../middleware/errorHandler');
+const validator = require('../middleware/validator');
 
 router.use([auth, errorHandler]);
 // router.use();
@@ -15,8 +15,8 @@ router.get('/addbooks', userController.getAddbook);
 router.get('/dashboard', userController.getDashboard);
 
 router.post('/saveprofile/:userId', userController.postSaveProfile);
-router.post('/addbooks', upload.single('image'), userController.postAddbook);
-router.post('/editbook/:bookId', upload.single('image'), userController.postEditBook);
+router.post('/addbooks', upload.single('image'), validator.bookFormValidate, userController.postAddbook);
+router.post('/editbook/:bookId', upload.single('image'), validator.bookFormValidate, userController.postEditBook);
 router.get('/removebook/:bookId', userController.getRemoveBook);
 
 module.exports = router;
