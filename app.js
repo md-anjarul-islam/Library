6395260c78516d810cbd705bdf01e748455d3e64
unknown <anjarulrobin@gmail.com>
2020-02-14 10:47:59 +0600

    Only the api part is remaining

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.set('views', path.join(__dirname , '/views')); // set express to look in this folder to render our view
-app.set('view engine', 'ejs'); // configure template engine
 app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
 
 app.use('/user', userRouter);
