a6096aa71f76ae537784efe399ffad3e0bcf9b33
Anjarul <anjarulrobin@gmail.com>
2020-02-26 11:54:48 +0600

    validator has been updated for schema validation which now returns a middleware

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const indexController = require('../controller/indexController');
-const validator = require('../middleware/validator');
+const indexController = require("../controller/indexController");
+const validator = require("../middleware/validator");
+const { loginSchema, registrationSchema } = require("../models/validationSchema");
 
-router.get('/', indexController.getHome);
-router.post('/login', validator.loginFormValidate, indexController.login);
-router.post('/register', validator.regFormValidate, indexController.register);
-router.get('/books', indexController.getAllBooks);
-router.get('/books/:bookId', indexController.getSingleBook);
+router.get("/", indexController.getHome);
+router.post("/login", validator(loginSchema), indexController.login);
+router.post("/register", validator(registrationSchema),indexController.register);
+router.get("/books", indexController.getAllBooks);
+router.get("/books/:bookId", indexController.getSingleBook);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controller/userController');
-const upload = require('../util/multer-upload');
-const auth = require('../middleware/auth');
-const {bookFormValidate, userUpdateFormValidate, bookUpdateFormValidate} = require('../middleware/validator');
-const {verify_user, verify_modifier} = require('../middleware/access_control');
+const userController = require("../controller/userController");
+const upload = require("../util/multer-upload");
+const auth = require("../middleware/auth");
+const validator = require("../middleware/validator");
+const { userUpdateSchema,  bookSchema,  bookupDateSchema } = require("../models/validationSchema");
+const { verify_user, verify_modifier } = require("../middleware/access_control");
 
 router.use(auth);
 
-router.get('/:userId', verify_user, userController.getProfile);
-router.patch('/:userId', verify_user, userUpdateFormValidate, userController.updateProfile);
-router.get('/:userId/books',verify_user, userController.getUserBooks);
-router.get('/:userId/books/:bookId', verify_user, verify_modifier, userController.getUsersSingleBook);
-router.post('/:userId/books', verify_user, upload.single('image'), bookFormValidate, userController.postAddbook);
-router.patch('/:userId/books/:bookId',verify_user, verify_modifier, upload.single('image'), bookUpdateFormValidate, userController.EditBook);
-router.delete('/:userId/books/:bookId',verify_user, verify_modifier, userController.RemoveBook);
+router.get("/:userId", verify_user, userController.getProfile);
+router.patch("/:userId", verify_user, validator(userUpdateSchema), userController.updateProfile);
+router.get("/:userId/books", verify_user, userController.getUserBooks);
+router.get("/:userId/books/:bookId", verify_user, verify_modifier, userController.getUsersSingleBook);
+router.post("/:userId/books", verify_user, upload.single("image"), validator(bookSchema), userController.postAddbook);
+router.patch("/:userId/books/:bookId", verify_user, verify_modifier, upload.single("image"),validator(bookupDateSchema), userController.EditBook);
+router.delete("/:userId/books/:bookId",verify_user,verify_modifier,userController.RemoveBook);
 
 module.exports = router;
