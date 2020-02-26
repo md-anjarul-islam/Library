ad5a5235408fbffa9823e1f2831cffa394452dc3
Anjarul <anjarulrobin@gmail.com>
2020-02-26 11:55:49 +0600

    Eslint changed the structure of code. nothing has been changed in code.

--- util/multer-upload.js
+++ util/multer-upload.js
@@ -1,28 +1,28 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 const filter = (req, file, cb) => {
-    const mimetype = file.mimetype;    
-    if(mimetype === 'image/jpeg' || mimetype === 'image/png'){
-        cb(null, true);
-    }else{
-        cb(null, false);
-    }
+  const mimetype = file.mimetype;
+  if (mimetype === "image/jpeg" || mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
 };
 
 const storage = multer.diskStorage({
-    destination(req, file, cb){
-        cb(null, path.join(__dirname, '/..', '/public/uploads/'));
-    },
-    filename(req, file, cb){        
-        cb(null, Date.now()+'-'+file.originalname);
-    }
+  destination(req, file, cb) {
+    cb(null, path.join(__dirname, "/..", "/public/uploads/"));
+  },
+  filename(req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
 });
 
 const upload = multer({
-    storage: storage,
-    fileFilter: filter,
-    limits: 1024*1024
+  storage: storage,
+  fileFilter: filter,
+  limits: 1024 * 1024
 });
 
 module.exports = upload;
