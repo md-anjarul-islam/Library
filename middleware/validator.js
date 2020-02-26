11143be23695bdf58d3a2d8b1f660cdb6eae542a
Anjarul <anjarulrobin@gmail.com>
2020-02-26 11:52:37 +0600

    Replaced some repetative functions using closure concept

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,69 +1,12 @@
-const Joi = require('@hapi/joi');
-const {loginSchema, registrationSchema, bookSchema, userUpdateSchema, bookupDateSchema} = require('../models/validationSchema');
-
-async function loginFormValidate(req, res, next){
-    try{
-        req.body = await loginSchema.validateAsync(req.body);
-        next();
+function validator(schema) {
+  return function(req, res, next) {
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+      let errMessage = error.details[0].message;
+      res.status(400).json({ message: errMessage });
+    } else {
+      next();
     }
-    catch(err){
-        let errMessage = getErrorMessages(err);        
-        res.status(400).json({message: errMessage});
-    }
+  };
 }
-
-async function regFormValidate(req, res, next){
-    try{
-        req.body = await registrationSchema.validateAsync(req.body);
-        next();
-    }
-    catch(err){
-        let errMessage = getErrorMessages(err);        
-        res.status(400).json({message: errMessage});
-    }
-}
-
-async function userUpdateFormValidate(req, res, next){
-    try{
-        req.body = await userUpdateSchema.validateAsync(req.body);
-        next();
-    }
-    catch(err){
-        let errMessage = getErrorMessages(err);        
-        res.status(400).json({message: errMessage});
-    }
-}
-
-async function bookFormValidate(req, res, next){
-    try{
-        req.body = await bookSchema.validateAsync(req.body);
-        next();
-    }
-    catch(err){
-        let errMessage = getErrorMessages(err);        
-        res.status(400).json({message: errMessage});
-    }
-}
-
-async function bookUpdateFormValidate(req, res, next){
-    try{
-        req.body = await bookupDateSchema.validateAsync(req.body);
-        next();
-    }
-    catch(err){
-        let errMessage = getErrorMessages(err);        
-        res.status(400).json({message: errMessage});
-    }
-}
-
-function getErrorMessages(err){
-    return err.details[0].message;
-}
-
-module.exports = {
-    loginFormValidate,
-    regFormValidate,
-    userUpdateFormValidate,
-    bookFormValidate,
-    bookUpdateFormValidate
-};
+module.exports = validator;
