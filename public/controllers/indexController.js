04487127902d37f91384b33512d9120fc79d2cc4
anjarul <anjarulrobin@gmail.com>
2020-03-09 00:28:25 +0600

    Change the navbar link when a user gets login or being registered

--- public/controllers/indexController.js
+++ public/controllers/indexController.js
@@ -1,57 +1,74 @@
-app.controller('indexController', function ($scope, $http) {
-    // $scope.message = "hello";
-    (function(){
-       $http({
-            method: "GET",
-            url: "books"
-        }).
-        then( (response) => {
-                if(response.data.length == 0)
-                    $scope.message = "No book found!";
-                else
-                    $scope.books = response.data;
-            } 
-        ).catch( (err) => {
-            $scope.message = err;
+app.controller("indexController", function($scope, $rootScope, $http) {
+
+
+    (function() {
+    $http({
+      method: "GET",
+      url: "books"
+    })
+      .then(response => {
+        if (response.data.length == 0) $scope.message = "No book found!";
+        else $scope.books = response.data;
+      })
+      .catch(err => {
+        $scope.message = err;
+      });
+  })();
+
+  $scope.login = function() {
+    let user = {
+      username: $scope.username,
+      password: $scope.password
+    };
+
+    $http({
+      method: "POST",
+      url: "/login",
+      data: JSON.stringify(user)
+    })
+      .then(response => {
+        let userInfo = response.data;
+        $rootScope.link2 = `#!/profile`;
+        $rootScope.link3 = "#!/dashboard";
+        $rootScope.link4 = "#!/logout";
+    
+        $rootScope.linkName2 = userInfo.fullname;
+        $rootScope.linkName3 = "Dashboard";
+        $rootScope.linkName4 = "Log Out";
+      })
+      .catch(err => {
+        $scope.userInfo = err.data.message;
+      });
+  };
+
+  $scope.register = function() {
+    if ($scope.password !== $scope.confirmpass)
+      $scope.message = "Please insert password correctly";
+    else {
+      let user = {
+        username: $scope.username,
+        fullname: $scope.fullname,
+        email: $scope.email,
+        password: $scope.password
+      };
+      $http({
+        method: "POST",
+        url: "/register",
+        data: JSON.stringify(user)
+      })
+        .then(response => {
+            let userInfo = response.data;
+            $rootScope.link2 = `#!/profile`;
+            $rootScope.link3 = "#!/dashboard";
+            $rootScope.link4 = "#!/logout";
+        
+            $rootScope.linkName2 = userInfo.fullname;
+            $rootScope.linkName3 = "Dashboard";
+            $rootScope.linkName4 = "Log Out";
+        })
+        .catch(err => {
+          $scope.message = err.data.message;
         });
-    })();
-
-    $scope.login = function(){
-        let user = {
-            username: $scope.username,
-            password: $scope.password
-        };
-
-        $http({
-            method: "POST",
-            url: "/login",
-            data: JSON.stringify(user)
-        }).then( (response)=>{
-            $scope.userInfo = response.data;
-        }).catch( (err)=> {
-            $scope.userInfo = err.data.message;
-        } );
     }
-
-    $scope.register = function(){
-        if($scope.password !== $scope.confirmpass)
-            $scope.message = "Please insert password correctly";
-        else{
-            let user = {
-                username: $scope.username,
-                fullname: $scope.fullname,
-                email: $scope.email,
-                password: $scope.password
-            };
-            $http({
-                method: "POST",
-                url: "/register",
-                data: JSON.stringify(user)
-            }).then( (response) => {
-                $scope.userInfo = response.data;
-            }).catch( (err)=> {
-                $scope.message = err.data.message;
-            })
-        }
-    }
+  };
 });
