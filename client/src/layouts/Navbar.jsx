9a7d5a219880bd231e4f240abfe4ece6b85747ca
Anjarul <anjarulrobin@gmail.com>
2020-05-06 15:16:11 +0600

    Put logic in separate functions

--- client/src/layouts/Navbar.jsx
+++ client/src/layouts/Navbar.jsx
@@ -1,41 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = (props) => {
+  console.log("Navbar");
+
+  return (
+    <NavbarUI>
+      <UnorderList>
+        <ListItem>
+          <form action="/searchbook" method="POST">
+            <input type="text" name="keyword" placeholder="search book..." />
+            <button type="submit"> Search </button>
+          </form>
+        </ListItem>
+
+        <ListItem>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+        </ListItem>
+      </UnorderList>
+    </NavbarUI>
+  );
+};
+
+const NavbarUI = ({ children }) => {
   return (
     <div
       className="top-bar p-3 mb-2 bg-dark"
       style={{ marginBottom: "30px !important" }}
     >
-      <ul className="nav justify-content-end">
-        <li className="nav-item">
-          <form action="/searchbook" method="POST">
-            <input type="text" name="keyword" placeholder="search book..." />
-            <button type="submit"> Search </button>
-          </form>
-        </li>
-
-        <li className="nav-item">
-          <Link className="nav-link" to="/">
-            {" "}
-            Home{" "}
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/login" className="nav-link">
-            {" "}
-            Login{" "}
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">
-            {" "}
-            Register{" "}
-          </Link>
-        </li>
-      </ul>
+      {children}
     </div>
   );
 };
 
+const UnorderList = ({ children }) => {
+  return <ul className="nav justify-content-end">{children}</ul>;
+};
+
+const ListItem = ({ children }) => {
+  return <li className="nav-item">{children}</li>;
+};
 export default Navbar;
