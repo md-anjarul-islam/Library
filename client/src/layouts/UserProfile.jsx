5a415b0ec58a7dd7add8cbc0e3ebb43f250e4732
Anjarul <anjarulrobin@gmail.com>
2020-05-06 15:18:36 +0600

    Use Card component to show the user information

--- client/src/layouts/UserProfile.jsx
+++ client/src/layouts/UserProfile.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { Link, Route, Redirect } from "react-router-dom";
+import LoginLayout from "./Login";
+import Card from "./Card";
 
 const Profile = (props) => {
-  console.log("in user profile", props);
-  return (
-    <div>
-      <h1>Profile page</h1>
-    </div>
-  );
+  console.log("User profile");
+
+  // console.log("match in user profile", props.match);
+
+  const user = props.location.state ? props.location.state.user : undefined;
+  if (user !== undefined)
+    return (
+      <Card title={"Profile page"}>
+        <h1>Welcome {user.fullname}</h1>
+        <p>{user.email}</p>
+        <Link to={`${props.match.url}/books`}>Show my books</Link>
+      </Card>
+    );
+  else return <Redirect to="/login"></Redirect>;
 };
 
 export default Profile;
