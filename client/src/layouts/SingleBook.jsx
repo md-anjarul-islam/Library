d9bc7ae37bda2c20f27581de5332540b9ca863e2
Anjarul <anjarulrobin@gmail.com>
2020-05-06 15:16:58 +0600

    Create component to show a single book

---
+++ client/src/layouts/SingleBook.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { Link, Route } from "react-router-dom";
+import axios from "axios";
+import Card from "./Card";
+
+const SingleBook = (props) => {
+  const [book, bookHandler] = useState({});
+  useEffect(() => {
+    let abortController = new AbortController();
+    axios({
+      method: "get",
+      url: `http://localhost:3001/api/books/${props.match.params.bookId}`,
+      headers: { token: localStorage.getItem("token") },
+    })
+      .then(({ data }) => {
+        console.log("single book", data);
+        bookHandler(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return () => abortController.abort();
+  }, []);
+
+  return (
+    <Card title={book.title} image={book.image}>
+      <h5>Author: {book.author}</h5>
+      {book.description && <fieldset>Description: {book.description}</fieldset>}
+      {book.rating && <p>Rating: {book.rating}</p>}
+    </Card>
+  );
+};
+
+export default SingleBook;
