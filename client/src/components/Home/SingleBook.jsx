73468c2c43be14969ea89d3998bb0e2c0dbb8ef0
Anjarul <anjarul.bs23@gmail.com>
2020-05-11 19:17:54 +0600

    Fix re-rendering component endlessly

--- client/src/components/Home/SingleBook.jsx
+++ client/src/components/Home/SingleBook.jsx
@@ -3,52 +3,6 @@ import { Link, Route } from "react-router-dom";
 import axios from "axios";
 import Card from "../../layouts/Card";
 import { fetchAPI, mainUrl } from "../../config";
-/* 
-const SingleBook = (props) => {
-  console.log("Single book mounting");
-  const [book, bookHandler] = useState({});
-  useEffect(() => {
-    let abortController = new AbortController();
-    axios({
-      method: "get",
-      url: `http://localhost:3001/api/books/${props.match.params.bookId}`,
-      headers: { token: localStorage.getItem("token") },
-    })
-      .then(({ data }) => {
-        console.log("single book", data);
-        bookHandler(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    return () => abortController.abort();
-  }, []);
-
-  return (
-    <Card title={book.title} image={book.image}>
-      <h5>Author: {book.author}</h5>
-      {book.description && <fieldset>Description: {book.description}</fieldset>}
-      {book.rating && <p>Rating: {book.rating}</p>}
-    </Card>
-  );
-};
- */
-
-async function getBook(bookId) {
-  axios({
-    url: `${mainUrl}/api/books/${bookId}`,
-    method: "get",
-  })
-    .then(({ data }) => {
-      console.log("component did mount".response);
-      // if (this._isMounted) this.setState({ book: data });
-      return data;
-    })
-    .catch((err) => {
-      console.log(err);
-      return err;
-    });
-}
 
 class SingleBook extends Component {
   constructor(props) {
@@ -73,24 +27,20 @@ class SingleBook extends Component {
       .catch((err) => console.log(err));
   }
 
-  async componentWillUpdate(prevProp, prevState) {
-    // console.log("COmponent updated", this.props);
-    // console.log("prev prop", prevProp, "next prop", prevState);
-    // console.log("this state", this.state.book);
-    axios({
-      url: `${mainUrl}/api/books/${this.props.match.params.bookId}`,
-      method: "get",
-    })
-      .then(({ data }) => {
-        console.log("component did mount", data);
-        console.log(
-          "comp",
-          JSON.stringify(this.state.book) == JSON.stringify(data)
-        );
-        if (JSON.stringify(this.state.book) != JSON.stringify(data))
-          if (this._isMounted) this.setState({ book: data });
-      })
-      .catch((err) => console.log(err));
+  async componentDidUpdate(prevProp, prevState) {
+    try {
+      let { data } = await axios({
+        url: `${mainUrl}/api/books/${this.props.match.params.bookId}`,
+        method: "get",
+      });
+      if (
+        this._isMounted &&
+        JSON.stringify(prevState.book) != JSON.stringify(data)
+      )
+        this.setState({ book: data });
+    } catch (err) {
+      console.log(err);
+    }
   }
   componentWillUnmount() {
     this._isMounted = false;
