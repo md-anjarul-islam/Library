842bc3887ea4e64d2498cdd50b28a96f53826096
Anjarul <anjarul.bs23@gmail.com>
2020-05-11 15:43:42 +0600

    Show single book by the side of all books

--- client/src/components/Home/SingleBook.jsx
+++ client/src/components/Home/SingleBook.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import { Link, Route } from "react-router-dom";
 import axios from "axios";
 import Card from "../../layouts/Card";
-
+import { fetchAPI, mainUrl } from "../../config";
+/* 
 const SingleBook = (props) => {
   console.log("Single book mounting");
   const [book, bookHandler] = useState({});
@@ -31,5 +32,76 @@ const SingleBook = (props) => {
     </Card>
   );
 };
+ */
 
+async function getBook(bookId) {
+  axios({
+    url: `${mainUrl}/api/books/${bookId}`,
+    method: "get",
+  })
+    .then(({ data }) => {
+      console.log("component did mount".response);
+      // if (this._isMounted) this.setState({ book: data });
+      return data;
+    })
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
+}
+
+class SingleBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { book: {} };
+    this._isMounted = true;
+  }
+
+  async componentDidMount() {
+    axios({
+      url: `${mainUrl}/api/books/${this.props.match.params.bookId}`,
+      method: "get",
+    })
+      .then(({ data }) => {
+        console.log("component did mount", data);
+        console.log(
+          "comp",
+          JSON.stringify(this.state.book) == JSON.stringify(data)
+        );
+        if (this._isMounted) this.setState({ book: data });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  async componentWillUpdate(prevProp, prevState) {
+    // console.log("COmponent updated", this.props);
+    // console.log("prev prop", prevProp, "next prop", prevState);
+    // console.log("this state", this.state.book);
+    axios({
+      url: `${mainUrl}/api/books/${this.props.match.params.bookId}`,
+      method: "get",
+    })
+      .then(({ data }) => {
+        console.log("component did mount", data);
+        console.log(
+          "comp",
+          JSON.stringify(this.state.book) == JSON.stringify(data)
+        );
+        if (JSON.stringify(this.state.book) != JSON.stringify(data))
+          if (this._isMounted) this.setState({ book: data });
+      })
+      .catch((err) => console.log(err));
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+    return (
+      <Card title={this.state.book.title} image={this.state.book.image}>
+        <p>Author: {this.state.book.author}</p>
+      </Card>
+    );
+  }
+}
 export default SingleBook;
