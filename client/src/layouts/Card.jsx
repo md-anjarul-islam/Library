4622f45be6a793478ad10d3d02c3298885627bbe
Anjarul <anjarul.bs23@gmail.com>
2020-05-11 15:44:11 +0600

    Use common style as reusable component

--- client/src/layouts/Card.jsx
+++ client/src/layouts/Card.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
 const Card = (props) => {
-  console.log("Card");
-
   return (
-    <div className="container" style={{ width: "400px" }}>
+    <div
+      className="container"
+      style={{ width: "400px", position: "sticky", top: 0 }}
+    >
       <div className="card shadow-lg">
-        <h5 style={{ textAlign: "center" }} className="card-header">
-          {props.title}
-        </h5>
-        <img
-          src={`http://localhost:3001/uploads/${props.image}`}
-          alt={props.title}
-        />
+        {props.title && (
+          <h5 style={{ textAlign: "center" }} className="card-header">
+            {props.title}
+          </h5>
+        )}
+        {props.image && (
+          <img
+            src={`http://localhost:3001/uploads/${props.image}`}
+            alt={props.title}
+          />
+        )}
         <div className="m-3 p-2">{props.children}</div>
       </div>
     </div>
