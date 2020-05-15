3695f9d2efe5562beac1f3fed97fa7f809ef0a66
Anjarul <anjarul.bs23@gmail.com>
2020-05-15 12:30:38 +0600

    Fix a small bug when deleting old-image

--- server/models/book.js
+++ server/models/book.js
@@ -10,7 +10,7 @@ const bookSchema = new db.Schema({
   rating: Number,
   seller: String,
   image: String,
-  modifier: String
+  modifier: String,
 });
 
 const Book = new db.model("Books", bookSchema);
@@ -25,81 +25,81 @@ async function findSingleBook(bookInfo) {
 }
 
 async function findUsersBook(user) {
-  try{
+  try {
     return await Book.find({ modifier: user._id });
-  }catch(err){
+  } catch (err) {
     return err;
   }
 }
 
 async function findAllBook() {
-  try{
+  try {
     return await Book.find();
-  }catch(err){
+  } catch (err) {
     return err;
   }
 }
 
 async function createBook(bookInfo, userId) {
-  try{
+  try {
     bookInfo.modifier = userId;
     const newBook = new Book(bookInfo);
     return await newBook.save();
-  }catch(err){
+  } catch (err) {
     return err;
   }
 }
 
 async function editBook(bookId, updatedBook) {
-  try{
+  try {
     const oldBook = await Book.findOne({ _id: bookId });
     if (updatedBook.image) deleteImage(oldBook.image);
-    else return await Book.updateOne({ _id: bookId }, { $set: updatedBook });
-  }catch(err){
+    return await Book.updateOne({ _id: bookId }, { $set: updatedBook });
+  } catch (err) {
     return err;
   }
 }
 
 async function removeBook(book) {
-  try{    
+  try {
     book = await Book.findOne(book);
     if (!book) return null;
-  
+
     deleteImage(book.image);
     return await Book.deleteOne({ _id: book._id });
-  }catch(err){
+  } catch (err) {
     return err;
   }
 }
 
 async function removeUserBook(user) {
-  try{
+  try {
     let books = await Book.find({ modifier: user._id });
     let message = "";
-    books.forEach(async book => {
+    books.forEach(async (book) => {
       deleteImage(book.image);
       message += await Book.deleteOne({ _id: book._id });
     });
-    return message;    
-  }catch(err){
+    return message;
+  } catch (err) {
     return err;
   }
 }
 
 async function searchBook(keyword) {
-  try{
+  try {
     return await Book.find().or([
       {
-        author: new RegExp(keyword)
+        author: new RegExp(keyword),
       },
       {
-        title: new RegExp(keyword)
+        title: new RegExp(keyword),
       },
       {
-        description: new RegExp(keyword)
-      }
-    ]);    
-  }catch(err){
+        description: new RegExp(keyword),
+      },
+    ]);
+  } catch (err) {
     return err;
   }
 }
@@ -108,7 +108,7 @@ function deleteImage(fileName) {
   if (!fileName) return null;
 
   const location = path.join(__dirname, "../", "public/uploads", fileName);
-  fs.unlink(location, err => {
+  fs.unlink(location, (err) => {
     if (err) console.log("Error deleting image file");
     else console.log("Image file deleted successfully");
   });
@@ -122,5 +122,5 @@ module.exports = {
   findSingleBook,
   findAllBook,
   findUsersBook,
-  removeUserBook
+  removeUserBook,
 };
