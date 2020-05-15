7e74a3f71c9dbdb75387fd9d1ade4015e970b1ce
Anjarul <anjarul.bs23@gmail.com>
2020-05-15 15:15:39 +0600

    Separate the single book and all book component
    - Creata separate route on separate page for single and all book
    component

--- client/src/components/User/index.jsx
+++ client/src/components/User/index.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import UserProfile from "./UserProfile";
 import UserBooks from "./UserBooks";
+import UserSingleBook from "./UserSingleBook"
 
 const User = (props) => {
   return (
     <Switch>
+      <Route
+          path={props.match.path+":id/books/:bookId"}
+          component={UserSingleBook}
+          props={props}
+        />
       <Route
         path={props.match.path + ":id/books"}
         props={props}
